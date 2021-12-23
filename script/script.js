4280050584a05a6ca6c26d07eefecf5b98763c5c
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-12-23 19:42:14 +0200

    Advanced JS 2.

--- script/script.js
+++ script/script.js
@@ -2,58 +2,24 @@
 'use strict';
 
 const mispelled = (str1, str2) => {
-  console.log('str1, str2: ', str1, str2);
+
   let count = 0;
-  let obj = {};
 
   for (let i = 0; i < str1.length; i++) {
 
-
-
+    if (str1[i] === str2[i] || str1[i] === str2[i + 1] || str1[i + 1] === str2[i]) {
+      count++;
+    }
 
   }
 
+  if (count === str1.length || count === str2.length ||
+    count === str1.length - 1 || count === str2.length - 1) {
+    return true;
+  } else {
+    return false;
+  }
 
-
-
-
-  /* 
-    if (str1.length + 1 <= str2.length || str1.length >= str2.length + 1) {
-      //str1.match(str2);count++;
-      if (!str1.match(str2) || !str2.match(str1)) {
-        console.log('----------str1: ', str1);
-        count++;
-      } else {
-        const match = str1.match(str2);
-        console.log('-----------------match: ', match.index);
-      }
-    }
-  
-    for (let i = 0; i < str1.length; i++) {
-  
-      if (str1[i] !== str2[i]) {
-  
-        count++;
-        if (str1[i] !== str2[i + 1]) {
-          count++;
-        } else {
-          count--;
-        }
-      }
-    }
-  
-  
-  
-  
-    if (count === 0 || count === 1) {
-      if (str1.length === str2.length || str1.length + 1 === str2.length || str1.length === str2.length + 1) {
-        return true;
-      } else if (str1.length + 1 <= str2.length || str1.length >= str2.length + 1) {
-        return false;
-      }
-    } else {
-      return false;
-    } */
 };
 
 console.log('mispelled => true', mispelled('versed', 'xersed'));
