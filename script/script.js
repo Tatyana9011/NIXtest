bde6497ff40217df68352a67c784830e0088628a
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-12-24 15:09:59 +0200

    AdvancedJS9

--- script/script.js
+++ script/script.js
@@ -1,37 +1,19 @@
 // eslint-disable-next-line strict
 'use strict';
 
-const addZero = n => n < 10 ? "0" + n : n;
+const daysRepresented = arr => {
+  let days = 0;
 
-const recursion = arrTime => {
-  if (+arrTime[2] >= 60) {
-    arrTime[2] = addZero(Math.floor(+arrTime[2] - 60));
-    arrTime[1] = addZero(Math.floor(+arrTime[1] + 1));
-  }
-  if (arrTime[1] >= 60) {
-    arrTime[1] = addZero(Math.floor(+arrTime[1] - 60));
-    arrTime[0] = addZero(Math.floor(+arrTime[0] + 1));
-  }
-  if (arrTime[0] >= 24) {
-    arrTime[0] = addZero(Math.floor(+arrTime[0] - 24));
-  }
+  arr.forEach(item => {
+    days += (item[1] - item[0]);
+  });
 
-  if (+arrTime[0] >= 24 || +arrTime[2] >= 60 || +arrTime[1] >= 60) {
-    recursion(arrTime);
-    return arrTime.join(':');
-  }
-  return arrTime.join(':');
+  return days + arr.length;
 };
 
-const timeCorrect = time => {
-  const arrTime = time.split(':');
+console.log('=> 17==', daysRepresented([[10, 15], [25, 35]]));
+console.log(' =>24 ==', daysRepresented([[2, 8], [220, 229], [10, 16]]));
+console.log('=> 16==', daysRepresented([[13, 20], [86, 93]]));
+console.log('=> 9==', daysRepresented([[1, 2], [8, 10], [12, 15]]));
+console.log(' =>22==', daysRepresented([[2, 8], [10, 16], [19, 26]]));
 
-  return recursion(arrTime);
-};
-
-console.log('=> 09:10:01==', timeCorrect('09:10:01'));
-console.log(' => 12:10:10==', timeCorrect('11:70:10'));
-console.log('=> 20:39:09==', timeCorrect('19:99:09'));
-console.log('=> 20:40:39==', timeCorrect('19:99:99'));
-console.log(' =>00:01:01==', timeCorrect('24:01:01'));
-console.log(' =>04:01:01==', timeCorrect('52:01:01'));
