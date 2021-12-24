85fe953161945e2d297a08813271a92e99f74c60
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-12-24 14:58:49 +0200

    AdvancedJS8

--- script/script.js
+++ script/script.js
@@ -1,38 +1,37 @@
 // eslint-disable-next-line strict
 'use strict';
 
-const solve = (str, num) => {
-  const arr = str.split('');
-  let count = 0;
+const addZero = n => n < 10 ? "0" + n : n;
 
-  for (let i = 0; i < str.length; i++) {
-
-    const regExp = new RegExp(str[i], 'g');
-
-    if (count < num) {
-
-      str.replace(regExp, (match, val) => {
-
-        if (count < num) {
-
-          arr[val] = '';
-          count++;
-
-        }
-
-      })
-    }
+const recursion = arrTime => {
+  if (+arrTime[2] >= 60) {
+    arrTime[2] = addZero(Math.floor(+arrTime[2] - 60));
+    arrTime[1] = addZero(Math.floor(+arrTime[1] + 1));
+  }
+  if (arrTime[1] >= 60) {
+    arrTime[1] = addZero(Math.floor(+arrTime[1] - 60));
+    arrTime[0] = addZero(Math.floor(+arrTime[0] + 1));
+  }
+  if (arrTime[0] >= 24) {
+    arrTime[0] = addZero(Math.floor(+arrTime[0] - 24));
   }
 
-  return arr.join('');
+  if (+arrTime[0] >= 24 || +arrTime[2] >= 60 || +arrTime[1] >= 60) {
+    recursion(arrTime);
+    return arrTime.join(':');
+  }
+  return arrTime.join(':');
 };
 
-console.log('=> bracadabra =====', solve('abracadabra', 1));
-console.log(' => brcadabra =====', solve('abracadabra', 2));
-console.log(
-  '=> rcdbr ===', solve('abracadabra', 6));
-console.log('=> сdr (в задании ошибка написано rdr))))===', solve('abracadabra', 8));
-console.log(' => ===', solve('abracadabra', 50));
+const timeCorrect = time => {
+  const arrTime = time.split(':');
 
+  return recursion(arrTime);
+};
 
-
+console.log('=> 09:10:01==', timeCorrect('09:10:01'));
+console.log(' => 12:10:10==', timeCorrect('11:70:10'));
+console.log('=> 20:39:09==', timeCorrect('19:99:09'));
+console.log('=> 20:40:39==', timeCorrect('19:99:99'));
+console.log(' =>00:01:01==', timeCorrect('24:01:01'));
+console.log(' =>04:01:01==', timeCorrect('52:01:01'));
