9daf697ddfc55dd20cb44d164832476a72ba41b7
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-12-24 08:55:52 +0200

    AdvancedJS4

--- script/script.js
+++ script/script.js
@@ -1,29 +1,27 @@
 // eslint-disable-next-line strict
 'use strict';
 
-const solve = str => {
-  const newStr = str.toLowerCase();
-  let count = 0;
+const example = (arr, num) => {
+  const newArr = [];
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] >= newStr[i]) {
-      count++;
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = i + 1; j < arr.length; j++) {
+
+      if ((arr[i] + arr[j]) === num) {
+        newArr.push(i, j);
+      }
+
     }
   }
 
-  if (count >= (str.length / 2)) {
-    str = str.toLowerCase();
-  } else {
-    str = str.toUpperCase();
-  }
-
-  return str;
+  return newArr;
 };
 
-console.log('=>code', solve('code'));
-console.log('=>CODE', solve('CODe'));
-console.log('=>code', solve('COde'));
-console.log('=>code', solve('Code'));
+console.log('=> [0, 1]', example([2, 7, 11, 15], 9));
+console.log(' => [0, 3]', example([12, 24, 34, 2], 14));
+console.log('=> [1, 3]', example([2, 7, 11, 15], 22));
+
 
 
 
