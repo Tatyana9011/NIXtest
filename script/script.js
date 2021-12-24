6750391ad2edce3bc8c1be6ea2b671120af9dba4
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-12-24 09:32:25 +0200

    AdvancedJS6

--- script/script.js
+++ script/script.js
@@ -1,24 +1,22 @@
 // eslint-disable-next-line strict
 'use strict';
 
-const findVowels = str => {
-  let count = 0;
-  const arr = [/а/g, /у/g, /е/g, /ы/g, /о/g, /э/g, /я/g, /и/g, /ю/g];
-  const newStr = str.toLowerCase();
+const solution = (str, num) => {
 
-  for (let i = 0; i < arr.length; i++) {
-    if (newStr.match(arr[i])) {
-      count += newStr.match(arr[i]).length;
-    }
+  let newStr = '';
+
+  if (str.length > num) {
+    newStr = str.substring(0, num) + '...';
+  } else {
+    newStr = str;
   }
 
-  return count;
+  return newStr;
 };
 
-console.log('=> 2 ', findVowels('Привет'));
-console.log(' => 5', findVowels('Абракадабра'));
-console.log('=> 8', findVowels('блаблаблаблоОООО'));
-console.log('=> 6', findVowels('ООблаблаблабло'));
+console.log('=> Tes... ', solution('Testing String', 3));
+console.log(' => Testing ...', solution('Testing String', 8));
+console.log('=> Test', solution('Test', 8));
 
 
 
